30a6914e5bee00148d6e5f800f17536d0934502c
nathanielh-vccp <nathanielh@vccp.com>
2014-04-04 17:16:42 +0100

    Lesson 2
    left and right

--- script.js
+++ script.js
@@ -1,34 +1,119 @@
-
+"use strict";
 // function run_right(){
-// 	//move slighty
-// 	console.log("hello")
-// 	setTimeout(function(){run_right();},200);
+// //move slighty
+// console.log("hello")
+// setTimeout(function(){run_right();},200);
 // }
 
-// run_right(1);
+//run_right();
 
-function run_right(slide){
+var timer;
+// Run right
+function run_right(slide, left)
+{
+	if ((left + 15) < (document.getElementById('stage').offsetWidth - document.getElementById('j').offsetWidth))
+	{
+
+		left = left + 15; // incearse his left attribute by 15px
+		document.getElementById('j').style.left = left+"px";
+
+		switch(slide){
+			//if slide equals 1
+			case 1:
+			document.getElementById('j').style.backgroundPosition = '-40px 0px';
+			timer = setTimeout(function(){run_right(2, left );},200);
+			break;
+			// if slide equals 2
+			case 2:
+			document.getElementById('j').style.backgroundPosition = '-80px 0px';
+			timer = setTimeout(function(){run_right(3, left );},200);
+			break;
+			// if slide equals 3
+			case 3:
+			document.getElementById('j').style.backgroundPosition = '-120px 0px';
+			timer = setTimeout(function(){run_right(4, left );},200);
+			break;
+			// if slide equals 4
+			case 4:
+			document.getElementById('j').style.backgroundPosition = '-80px 0px';
+			timer = setTimeout(function(){run_right(1, left );},200);
+			break;
+		}
+	}else{
+		document.getElementById('j').style.backgroundPosition = '0px 0px';
+	}
+}
+
+function stop_running(){
+	document.getElementById('j').style.backgroundPosition = '0px 0px';
+	clearTimeout(timer);
+}
+
+//run left
+function run_left(slide, left)
+{
+	if ((left - 15) > 0)
+	{
+		left = left + 15; // incearse his left attribute by 15px
+		document.getElementById('j').style.left = left+"px";
+
+		switch(slide){
+			//if slide equals 1
+			case 1:
+			document.getElementById('j').style.backgroundPosition = '-40px -50px';
+			timer = setTimeout(function(){run_left(2, left );},200);
+			break;
+			// if slide equals 2
+			case 2:
+			document.getElementById('j').style.backgroundPosition = '-80px -50px';
+			timer = setTimeout(function(){run_left(3, left );},200);
+			break;
+			// if slide equals 3
+			case 3:
+			document.getElementById('j').style.backgroundPosition = '-120px -50px';
+			timer = setTimeout(function(){run_left(4, left );},200);
+			break;
+			// if slide equals 4
+			case 4:
+			document.getElementById('j').style.backgroundPosition = '-80px -50px';
+			timer = setTimeout(function(){run_left(1, left );},200);
+			break;
+		}
+	}else{
+		document.getElementById('j').style.backgroundPosition = '0px -50px';
+	}
+}
+
+
+
+/*
+function run_right(slide, left)
+{
+	left = left + 15; // incearse his left attribute by 15px
+	document.getElementById('j').style.left = left+"px";
+
 	switch(slide){
 		//if slide equals 1
 		case 1:
-		document.getElementById('j').style.backgroundPosition = '-40px 0px'
-		setTimeout(function(){run_right(2);},200);
+		document.getElementById('j').style.backgroundPosition = '-40px 0px';
+		setTimeout(function(){run_right(2, left );},200);
 		break;
 		// if slide equals 2
 		case 2:
-		document.getElementById('j').style.backgroundPosition = '-80px 0px'
-		setTimeout(function(){run_right(3);},200);
+		document.getElementById('j').style.backgroundPosition = '-80px 0px';
+		setTimeout(function(){run_right(3, left );},200);
 		break;
 		// if slide equals 3
 		case 3:
-		document.getElementById('j').style.backgroundPosition = '-120px 0px'
-		setTimeout(function(){run_right(4);},200);
+		document.getElementById('j').style.backgroundPosition = '-120px 0px';
+		setTimeout(function(){run_right(4, left );},200);
 		break;
 		// if slide equals 4
 		case 4:
-		document.getElementById('j').style.backgroundPosition = '-80px 0px'
-		setTimeout(function(){run_right(1);},200);
+		document.getElementById('j').style.backgroundPosition = '-80px 0px';
+		setTimeout(function(){run_right(1, left );},200);
 		break;
 
 	}
 }
+*/
